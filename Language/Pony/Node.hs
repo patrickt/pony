e9e255dbbf4dd9bfee826c0b5a504191f33325f7
Patrick Thomson <pthomson@apple.com>
2010-09-05 18:54:49 -0400

    wait, perhaps type families aren't what I wanted.

--- Language/Pony/Node.hs
+++ Language/Pony/Node.hs
@@ -2,18 +2,24 @@
 
 module Language.Pony.Node where
   
+  import Control.Applicative
   import Language.C.Data.Node
   import Language.C.Syntax.AST
   import Language.C.Syntax.Constants
     
-  -- The Concrete data family represents things that can be compiled down to CNodes.
+  -- The Concrete typeclass represents things that can be compiled down to C expressions.
   -- At the end of all of the transformations, every node in the syntax tree must 
-  -- be a Concrete node. The ConcreteRep type is necessary 
+  -- be a Concrete node. New functions and builtins can be provided by implementing new 
+  -- instances of Concrete.
   class Concrete a where
-    type CNodelike :: *
-    toCNode :: a -> CNodelike
+    toExpr :: a -> CExpr
+  
+  -- All C expressions are representable in a concrete manner, obviously.
+  instance Concrete CExpr where toExpr = id
+  
+  -- But so are all Haskell Ints.
+  instance Concrete Int where
+    toExpr i = CConst $ CIntConst boosted internalNode where boosted = cInteger . toInteger $ i
     
-  instance Concrete Integer where
-    type CNodelike = CConst
-    toCNode x = CIntConst (cInteger x) internalNode
     
+  
