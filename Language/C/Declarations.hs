335991f84eb37d5a75fa31e64d484ac0a19d7b55
Patrick Thomson <pthomson@apple.com>
2010-11-15 11:54:11 -0500

    Cleaned up the declaration parser, but still worried that I'm writing myself into a corner here.

--- Language/C/Declarations.hs
+++ Language/C/Declarations.hs
@@ -36,7 +36,7 @@ where
     c <- L.brackets constant
     return $ Array [] c
   
-
+  
   -- TODO: enforce that the binary operator has to be an assignment
   -- BUG: array declarations are broken again
   declaration :: Parser CDeclaration
@@ -47,16 +47,11 @@ where
     -- there should be a parsing failure if you don't specify a type qualifier
     ptrs <- many derived
     expr <- expression
-    case expr of
-      (Identifier ident) -> do
-        arr <- optionMaybe array
-        let dervs = if arr == Nothing then ptrs else fromJust arr : ptrs
-        L.semi
-        return $ TopLevel typs (Named ident dervs) Nothing
-      (BinaryOp eq (Identifier lhs) rhs) -> do
-        L.semi
-        return $ TopLevel typs (Named lhs []) (Just rhs)
-      (Call (Identifier name) rhs) -> do
-        L.semi
-        return $ TopLevel typs (Named name [Function]) Nothing
-      otherwise -> fail "whoops"
+    let result = peruseExpression typs ptrs expr
+    L.semi
+    result
+    where
+      peruseExpression typs ptrs (Identifier ident) = return $ TopLevel typs (Named ident ptrs) Nothing
+      peruseExpression typs ptrs (BinaryOp eq (Identifier lhs) rhs) = return $ TopLevel typs (Named lhs ptrs) (Just rhs)
+      peruseExpression typs ptrs (Call (Identifier name) rhs) = return $ (TopLevel typs (Named name (Function : ptrs)) Nothing)
+      peruseExpression _ _ _ = fail "invalid declarator or bug (the latter is more likely)"
