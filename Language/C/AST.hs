643b2c17f6cabdb5bdfb5d0cd663679d29e37314
Patrick Thomson <pthomson@apple.com>
2011-02-03 12:00:48 -0500

    Finish documenting Language.C.AST.

--- Language/C/AST.hs
+++ Language/C/AST.hs
@@ -5,7 +5,6 @@ module Language.C.AST
   where
   
   import Data.Either
-  import Text.Printf
   import Data.Generics
   
   -- TODO: Add position information to all of the types, etc.
@@ -16,7 +15,7 @@ module Language.C.AST
     = [CExternal]
   
   -- | A block item is either a declaration or statement (C99 6.8.3).
-  -- | Block items make up the bodies of compound statements.
+  -- Block items make up the bodies of compound statements.
   type BlockItem 
     = Either CDeclaration CStatement
   
@@ -68,10 +67,13 @@ module Language.C.AST
     deriving (Eq, Show, Typeable, Data)
   
   -- TODO: Expand this to include __builtin_offsetof and __builtin_types_compatible_p
+  -- | GNU/clang built-in functions that are exposed after preprocessing.
   data BuiltinExpr
+    -- | Corresponds to @__builtin_va_arg(id, type)@.
     = BuiltinVaArg CExpr CDeclaration
     deriving (Eq, Show, Typeable, Data)
   
+  -- | C literals.
   data CLiteral
     = CInteger Integer
     | CChar Char
@@ -85,6 +87,8 @@ module Language.C.AST
     show (CFloat f) = show f
     show (CString s) = s
   
+  -- | Storage class specifiers (C99 6.7.1).
+  -- As an extension, @__attribute__(())@ is considered a storage specifier.
   data StorageSpecifier
     = SAuto
     | SRegister
@@ -93,31 +97,31 @@ module Language.C.AST
     | STypedef
     | SAttribute CAttribute
     deriving (Eq, Show, Typeable, Data)
-    
+  
+  -- | Type qualifiers (C99 6.7.3) and function specifiers (C99 6.7.4).
+  -- Please note that the 'FInline' qualifier must only be applied to functions.
   data TypeQualifier
     = QConst
     | QRestrict
     | QVolatile
     | FInline
-    deriving (Eq, Typeable, Data)
+    deriving (Eq, Show, Typeable, Data)
   
-  instance Show TypeQualifier where
-    show QConst = "const"
-    show QRestrict = "restrict"
-    show QVolatile = "volatile"
-    show FInline = "inline"
-  
+  -- | C qualifiers and specifiers.
   data Specifier 
     = TSpec TypeSpecifier
     | TQual TypeQualifier
     | SSpec StorageSpecifier
     deriving (Eq, Show, Typeable, Data)
-    
+  
+  -- | C enumeration specifiers (C99 6.7.2.2).
   data Enumerator 
     = EnumIdent String
     | EnumAssign String CExpr
     deriving (Eq, Show, Typeable, Data)
   
+  -- | C type specifiers (6.7.2).
+  -- As a GNU extension, @typeof(expr)@ is supported.
   data TypeSpecifier
      = TVoid
      | TChar
@@ -129,6 +133,7 @@ module Language.C.AST
      | TSigned
      | TUnsigned
      | TBool
+     -- | Corresponds to the @__builtin_va_arg@ type.
      | TBuiltin String
      | TStructOrUnion (Maybe String) Bool [CDeclaration] [CAttribute]
      | TEnumeration (Maybe String) [Enumerator] [CAttribute]
@@ -136,29 +141,40 @@ module Language.C.AST
      | TTypeOfExpr CExpr
      deriving (Eq, Show, Typeable, Data)
   
+  -- | C @__attribute__(())@ specifications. 
   data CAttribute = CAttribute [CExpr]
     deriving (Eq, Show, Typeable, Data)
   
+  -- TODO: Remove this, as it is no longer necessary
   data CSize 
     = Sized CExpr
     deriving (Eq, Show, Typeable, Data)
   
+  -- | C declarations (C99 6.7).
+  -- Though this definition is rather byzantine in its structure, it has 
+  -- distinct advantage in that it can encapsulate structure declarations,
+  -- parameter declarations, and type names. This method of structuring declarations 
+  -- was innovated by Benedikt Huber.
   data CDeclaration 
     = CDeclaration [Specifier] [(Maybe CDeclarator, Maybe Initializer, Maybe CSize)]
     deriving (Eq, Show, Typeable, Data)
     
   type AsmName = Maybe String
   
+  -- TODO: Unify this declaration.
+  -- | C declarators (C99 6.7.5 and 6.7.6).
   data CDeclarator
    = Named String [DerivedDeclarator] AsmName [CAttribute]
    | Abstract [DerivedDeclarator] [CAttribute]
    deriving (Eq, Show, Typeable, Data)
   
+  -- | Initialization types can contain one expression or a bracketed list of initializers.
   data Initializer 
     = InitExpression CExpr
     | InitList [Initializer]
     deriving (Eq, Show, Typeable, Data)
   
+  -- | Indirectly derived declarators used inside the 'CDeclarator' type.
   data DerivedDeclarator
    = Pointer [TypeQualifier]
    | Array [TypeQualifier] (Maybe CExpr)
