343a5521636f37802e8ccc30d51cbb0033570d5a
Patrick Thomson <pthomson@apple.com>
2011-02-10 14:57:19 -0500

    Add a checked-malloc transformation that works on declarations and on plain assignments.

--- Language/Pony/CheckMalloc.hs
+++ Language/Pony/CheckMalloc.hs
@@ -1,9 +1,24 @@
 module Language.Pony.CheckMalloc where
   
+  import Control.Monad
   import Language.C
   import Semantics.C.Nodes
+  import Data.List
+  import Data.Generics
+  import Data.Generics.Zipper
+  import Data.Maybe
+  import Debug.Trace
   
-  checkMallocs :: Statement -> Statement
-  checkMallocs (ExpressionS call@(Binary left "=" (FunctionCall (Ident "malloc") args))) =
-    IfThen (Unary "!" call) (Return (Just (Literal (CInteger 1)))) 
-  checkMallocs other = other
+  checkMallocDeclarations :: [SLocal] -> [SLocal]
+  checkMallocDeclarations = concatMap convert where
+    convert d@(LDeclaration (Variable name _ (Just (FunctionCall (Ident "malloc") _)))) =
+      [ d, LStatement (IfThen (Unary "!" (Ident name)) (ExpressionS (FunctionCall (Ident "abort") []))) ]
+    convert x = [x]
+  
+  checkMallocAssignments :: Statement -> Statement
+  checkMallocAssignments (ExpressionS call@(Binary left "=" (FunctionCall (Ident "malloc") args))) =
+    (IfThen (Unary "!" call) (ExpressionS (FunctionCall (Ident "abort") [])))
+  checkMallocAssignments x = x
+  
+  checkMalloc :: GenericT
+  checkMalloc = (mkT checkMallocAssignments) `extT` checkMallocDeclarations
--- Repl.hs
+++ Repl.hs
@@ -3,11 +3,12 @@ module Main where
   import Text.Parsec hiding (parseTest)
   import Language.C
   import Semantics.C
+  import Data.Generics
   import Data.Generics.Zipper
   import Language.Pony.CheckMalloc
   import System.IO.Unsafe
   
-  prog = "int main(int argc, char const** argv){ char *buffer = malloc(sizeof(char) * 4096); printf(\"Successfully allocated memory\\n\"); return 0; }"
+  prog = "int main() { char *buffer = malloc(sizeof(char) * 4096); char* otherbuffer; otherbuffer = malloc(2049); return 0;}"
   
   ast = parseUnsafe functionDefinition prog
   
