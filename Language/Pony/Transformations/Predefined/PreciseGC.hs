71060b3bb6e99ec701f728012dfafa8f21ef97aa
Patrick Thomson <pthomson@apple.com>
2011-04-04 10:49:47 -0400

    update the precise GC transformation and make .gitignore ignore 'a.out'.

--- Language/Pony/Transformations/Predefined/PreciseGC.hs
+++ Language/Pony/Transformations/Predefined/PreciseGC.hs
@@ -2,6 +2,31 @@ module Language.Pony.Transformations.Predefined.PreciseGC where
   
   import Semantics.C
   import Data.Generics
+  import Data.List
+  
+  -- Avert your gaze!
+  list_type = (SPointerTo (Typedef "list_t" (SComposite (CompositeInfo 
+    Struct (Just "list_s") [SField (Just "next") (SPointerTo 
+    (SComposite (CompositeInfo Struct (Just "list_s") []) []) []) 
+    Nothing,SField (Just "tag") (Typedef "tag_t" (SEnum (EnumerationInfo Nothing 
+    [Enumeration "LIST" Nothing,Enumeration "VALUE" Nothing]) []) []) 
+    Nothing,SField (Just "c") (SComposite (CompositeInfo Union 
+    (Just "ldata") []) []) Nothing]) []) []) [])
+  
+  -- the post-processed representation of NULL.
+  -- I don't like it either.
+  sNull = SCast (SPointerTo (SVoid []) []) (intToLiteral 0)
+  
+  -- Even if I wasn't headed to hell, this would get me an assigned seat.
+  insertGCList :: [SGlobal] -> [SGlobal]
+  insertGCList x = 
+    let 
+      gc_list = GVariable (Variable "gc_list" list_type (Just sNull))
+      free_list = GVariable (Variable "free_list" list_type (Just sNull))
+      convert nil@(GVariable (Variable "nil" _ _)) = [nil, gc_list, free_list]
+      convert x = [x]
+    in 
+      if (elem gc_list x) then x else concatMap convert x
   
   -- make `del` a no-op
   precise :: SFunction -> SFunction
@@ -10,4 +35,4 @@ module Language.Pony.Transformations.Predefined.PreciseGC where
   precise x = x
   
   gcT :: GenericT
-  gcT = mkT precise
+  gcT = mkT precise `extT` insertGCList
