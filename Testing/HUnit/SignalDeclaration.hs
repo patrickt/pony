2ec784fe207a227e3aa60d4b86d767a9ffd9bfab
Patrick Thomson <pthomson@apple.com>
2011-04-04 10:48:43 -0400

    add a failing test for issue #41.

---
+++ Testing/HUnit/SignalDeclaration.hs
@@ -0,0 +1,18 @@
+module Testing.HUnit.SignalDeclaration 
+  ( tests ) where
+  
+  import Test.Framework
+  import Test.Framework.Providers.HUnit
+  import Test.HUnit hiding (Test)
+  import Language.C
+  import Semantics.C
+  
+  tests :: [Test]
+  tests = [ testCase "signal(3) declaration" testSignal
+          ]
+  
+  testSignal :: Assertion
+  testSignal = assertEqual "signal(3)" practice theory
+    where theory = convert $ parseUnsafe preprocessedC "void (*signal(int sig, void (*func)(int)))(int);"
+          practice = [GVariable $ Variable "signal" (SFunctionPointer void [(SParameter (Just "sig") signedInt), 
+                                                                           (SParameter (Just "func") (SFunctionPointer void [(SParameter Nothing signedInt)] []))] []) Nothing]
--- Testing/Main.hs
+++ Testing/Main.hs
@@ -6,12 +6,14 @@ module Main where
   import qualified Testing.QuickCheck.Constants as QC
   import qualified Testing.HUnit.SpaceFreeParsing as SFP
   import qualified Testing.HUnit.PostfixExpressions as PE
+  import qualified Testing.HUnit.SignalDeclaration as SD
   
   tests :: [Test]
   tests = [ testGroup "HUnit: type names" TN.tests
           , testGroup "QuickCheck: constants" QC.tests
           , testGroup "HUnit: space-free parsing" SFP.tests
           , testGroup "HUnit: postfix expressions" PE.tests
+          , testGroup "HUnit: signal(3) declaration" SD.tests
           ]
   
   main :: IO ()
