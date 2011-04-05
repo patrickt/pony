8a6d821a59571196b99c381686a9fc529bbf55d6
Patrick Thomson <pthomson@apple.com>
2011-04-05 13:18:09 -0400

    fix an issue with OS X's ld and continue working on the GC transformation

--- Language/Pony/Transformations/Predefined/PreciseGC.hs
+++ Language/Pony/Transformations/Predefined/PreciseGC.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Language.Pony.Transformations.Predefined.PreciseGC where
   
+  import GHC.Exts ( IsString(..) )
   import Semantics.C
   import Data.Generics
   import Data.List
+  import Control.Applicative
+  
+  instance IsString Expression where fromString = Ident
+  
+  countWhere :: (a -> Bool) -> [a] -> Int
+  countWhere pred x = length $ filter pred x
   
   -- Avert your gaze!
   list_type = (SPointerTo (Typedef "list_t" (SComposite (CompositeInfo 
@@ -21,12 +30,30 @@ module Language.Pony.Transformations.Predefined.PreciseGC where
   insertGCList :: [SGlobal] -> [SGlobal]
   insertGCList x = 
     let 
-      gc_list = GVariable (Variable "gc_list" list_type (Just sNull))
-      free_list = GVariable (Variable "free_list" list_type (Just sNull))
+      gc_list = GVariable (Variable "all_lists" list_type (Just sNull))
+      free_list = GVariable (Variable "marked" (SPointerTo signedInt []) (Just sNull))
       convert nil@(GVariable (Variable "nil" _ _)) = [nil, gc_list, free_list]
       convert x = [x]
     in 
       if (elem gc_list x) then x else concatMap convert x
+      
+  referencedListCount :: SFunction -> Int
+  referencedListCount (SFunction _ _ _ params locals _) = countParams params + countLocals locals where
+    countParams p = countWhere isListParameter p
+    isListParameter (SParameter _ t) = t == list_type
+    countLocals _ = 0
+    
+  addParameterCount :: SFunction -> SFunction
+  addParameterCount f@(SFunction attrs typ name params locals isVariadic) = 
+    SFunction attrs typ name params (pcount : locals) isVariadic where
+      pcount = LDeclaration $ Variable "parameter_count" signedInt (Just <$> intToLiteral $ referencedListCount f)
+  
+  modifyMain :: SFunction -> SFunction
+  modifyMain (SFunction attrs typ "main" params locals isVariadic) = 
+    SFunction attrs typ "main" params (start ++ (init locals) ++ end) isVariadic where
+      start = [ LStatement $ ExpressionS (Binary "marked" "=" (FunctionCall "malloc" [Binary (intToLiteral 35565) "*" (SizeOfSType signedInt)])) ] 
+      end = [ LStatement $ Return $ Just $ intToLiteral 0 ]
+  modifyMain x = x
   
   -- make `del` a no-op
   precise :: SFunction -> SFunction
@@ -35,4 +62,4 @@ module Language.Pony.Transformations.Predefined.PreciseGC where
   precise x = x
   
   gcT :: GenericT
-  gcT = mkT precise `extT` insertGCList
+  gcT = mkT precise `extT` insertGCList `extT` modifyMain `extT` addParameterCount
