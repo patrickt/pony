038546454780f510001d263438e1f29aced2bb33
Patrick Thomson <pthomson@apple.com>
2011-08-24 21:31:40 -0400

    Introduce the new style of writing Pony projects. From this point, I will be deprecating the old .ponyproj style.

---
+++ Concat.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Concat where
+  
+  import Language.Pony
+  
+  strlen l = FunctionCall (Ident "strlen") [Ident l]
+  
+  malloc l s = stmt $ ((Ident l) .=. (FunctionCall "alloca") [s])
+  
+  strlcpy to from size = stmt $ FunctionCall (Ident "strlcpy") [to, from, size]
+  strlcat to from size = stmt $ FunctionCall (Ident "strlcat") [to, from, size]
+  
+  checkForConcatenation :: SLocal -> [SLocal]
+  checkForConcatenation (LStatement (ExpressionS (Binary (Ident a) "=" (Binary (Ident l) "<+>" (Ident r))))) = 
+    [ LDeclaration (SVariable "needed_size" (Typedef "size_t" unsignedInt []) 
+        (Just (Binary (strlen l) "+" (strlen r))))
+    , malloc a (Ident "needed_size")
+    , strlcpy (Ident a) (Ident l) (Ident "needed_size")
+    , strlcat (Ident a) (Ident r) (Ident "needed_size")
+    ]
+  checkForConcatenation x = [x]
+  
+  concatT :: GenericT
+  concatT = mkT $ concatMap checkForConcatenation
+  
+  main :: IO ()
+  main = run $ pony {
+    operators = [Arithmetic "<+>"],
+    transformations = [ MkTrans "StringConcat" TopDown concatT ]
+  }
--- Language/Pony.hs
+++ Language/Pony.hs
@@ -1,3 +1,69 @@
-module Language.Pony where
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Language.Pony 
+  ( module Data.Generics
+  , module Language.Pony.Transformations
+  , module Language.Pony.Transformations.Utilities
+  , module Semantics.C
+  , run
+  , pony
+  , Operator (..)
+  , PonyOptions (..)
+  ) 
   
+  where
   
+  import Data.Generics hiding (empty)
+  import Language.C
+  import Language.Pony.Transformations
+  import Language.Pony.Transformations.Utilities
+  import Semantics.C
+  import System.Environment
+  
+  data Operator 
+    = Arithmetic String
+    | Bitwise String 
+    | Comparative String
+    | Logical String
+  
+  run :: PonyOptions -> IO ()
+  run PonyOptions { output, operators, transformations } = do
+    -- todo support more than one output file.
+    args <- getArgs
+    let input = last args
+    let internals = internalsFromOperators operators
+    let trans = head transformations
+    result <- preprocessAndParse preprocessedC input internals
+    case result of
+      (Left parseError) -> print parseError
+      Right externs -> do
+        let converted = convert externs
+        let (MkTrans _ direction t) = trans
+        case direction of 
+          TopDown -> writeFile output (show $ pretty $ everywhere t converted)
+          BottomUp -> writeFile output (show $ pretty $ everywhere' t converted)
+  
+  internalsFromOperators :: [Operator] -> Internals
+  internalsFromOperators ops = emptyInternals {
+    arithmeticOps = ariths,
+    comparativeOps = compars,
+    bitwiseOps = bitwises,
+    logicalOps = logics
+  } where 
+    ariths = [ a | (Arithmetic a) <- ops ]
+    compars = [ c | (Comparative c) <- ops ]
+    bitwises = [ b | (Bitwise b) <- ops ]
+    logics = [ q | (Logical q) <- ops ]
+  
+  data PonyOptions = PonyOptions 
+    { output :: FilePath
+    , operators :: [Operator]
+    , transformations :: [Transformation]
+    }
+  
+  pony :: PonyOptions
+  pony = PonyOptions 
+    { output          = "/dev/stdout"
+    , operators       = []
+    , transformations = []
+    }
