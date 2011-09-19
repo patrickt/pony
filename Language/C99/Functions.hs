150eede16958b7957472d596cee979114a9c43c5
Patrick Thomson <pthomson@apple.com>
2011-09-19 13:21:07 -0400

    Butcher the definition of functions, but fix issue #31 in the process.

--- Language/C99/Functions.hs
+++ Language/C99/Functions.hs
@@ -8,6 +8,8 @@ module Language.C99.Functions where
   
   -- | C function definition (C99 6.9.1).
   functionDefinition :: Parser CFunction
-  functionDefinition = pure CFunction <*> some specifier
-                                      <*> declarator
-                                      <*> compoundStmt
+  functionDefinition = do 
+    oldState <- getState
+    func <- CFunction <$> some specifier <*> declarator <*> compoundStmt
+    putState oldState
+    return func
