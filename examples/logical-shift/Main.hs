37604ed5ad2e18812c37d48925871ad2d4a1a378
Patrick Thomson <patrick.william.thomson@gmail.com>
2013-04-21 14:25:07 -0700

    fix logical shift example

--- examples/logical-shift/Main.hs
+++ examples/logical-shift/Main.hs
@@ -1,32 +1,19 @@
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE ViewPatterns, OverloadedStrings #-}
 
 module Main where
   
   import Language.Pony
-  import Language.C99.Literals
-  
-  op :: String -> Fix Sem -> Fix Sem -> Fix Sem
-  op n a b = tie $ Binary a (name' n) b
-  
-  infixl 7 .>>.
-  (.>>.) = op ">>"
-  
-  int_ = tie $ IntT (tie Signed) nil'
-  
-  uint_ = tie $ IntT (tie Unsigned) nil'
-  
-  paren x = tie $ Paren x
-  
-  cast :: Fix Sem -> Fix Sem -> Fix Sem
-  cast typ arg = tie $ Cast typ arg
+    
+  infixl 8 .>>.
+  a .>>. b = binary' a ">>" b
   
   -- bug: need to evaluate the arguments here...
-  convertLogicalShift :: Fix Sem -> Sem (Fix Sem)
-  convertLogicalShift (µ -> (Binary l (µ -> Name ">>>") r)) = out $ cast int_ $ paren $ cast uint_ $ l .>>. r
+  convertLogicalShift :: CSyn -> C99 CSyn
+  convertLogicalShift (µ -> (Binary l (µ -> Name ">>>") r)) = µ $ cast' int' $ paren' $ cast' (unsigned' int') $ l .>>. r
   convertLogicalShift other = out other
   
   main :: IO ()
   main = run $ def 
-    { bitwiseOperators = [">>>"]
-    , anamorphisms = [convertLogicalShift]
+    { binaryOperators = defaultOperators ++ [ cautiousBinaryOp ">>>" 8 ]
+    , anamorphisms = [ convertLogicalShift ]
     }
