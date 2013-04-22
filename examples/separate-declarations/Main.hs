53e8099115022248a3285662e18392622d9ffe3c
Colin Barrett <colin@springsandstruts.com>
2013-04-21 19:47:36 -0700

    Update separate declarations example

--- examples/separate-declarations/Main.hs
+++ examples/separate-declarations/Main.hs
@@ -1,29 +1,25 @@
+{-# LANGUAGE ViewPatterns, OverloadedStrings #-}
 module Main where
   
   import Language.Pony
   
-  declare :: String -> SType -> Local
-  declare n t = LDeclaration (Variable n t Nothing)
+  partitionStatements :: [CSyn] -> ([CSyn], [CSyn])
+  partitionStatements xs = partition xs ([], [])
+    where partition (v@(µ -> Variable _ _ Nothing) : rest) (a,b) =
+            partition rest (a ++ [v], b)
+          partition (v@(µ -> Variable t n (Just e)) : rest) (a,b) =
+            partition rest (a ++ [variable' t n Nothing],
+                            b ++ [binary' n "=" e])
+          partition (other : rest) (a, b) =
+            partition rest (a, b ++ [other])
+          partition [] them = them
   
-  partitionLocals :: [Local] -> ([Local], [Local])
-  partitionLocals ls = partition ls ([], []) 
-    where 
-      partition [] them = them
-      partition (d@(LDeclaration (Variable _ _ Nothing)) : rest) (a,b) = 
-        partition rest (a ++ [d], b)
-      partition (s@(LStatement st) : rest) (a, b) = 
-        partition rest (a, b ++ [s])
-      partition (d@(LDeclaration (Variable n t (Just e))) : rest) (a,b) =
-        partition rest (a ++ [declare n t], 
-                        b ++ [stmt $ Ident n .=. e])
-  
-  separate :: [Local] -> [Local]
-  separate xs = a ++ b where (a, b) = partitionLocals xs
-  
-  separateT :: GenericT
-  separateT = mkT separate
+  separate :: CSyn -> C99 CSyn
+  separate (µ -> Group xs) = Group $ a ++ b
+    where (a, b) = partitionStatements xs
+  separate other = out other
   
   main :: IO ()
-  main = run $ pony {
-    transformations = [MkTrans "SeparateDeclarations" TopDown separateT]
-  }
+  main = run $ def 
+    { anamorphisms = [ separate ]
+    }
