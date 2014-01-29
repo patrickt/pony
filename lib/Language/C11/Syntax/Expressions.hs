7937f6a31a6e30c7fe9304a20be13ec3a211ed4e
Patrick Thomson <patrick.william.thomson@gmail.com>
2014-01-28 21:26:36 -0500

    final design for Expressions

--- lib/Language/C11/Syntax/Expressions.hs
+++ lib/Language/C11/Syntax/Expressions.hs
@@ -1,18 +1,20 @@
 module Language.C11.Syntax.Expressions where
 
+  import Language.Pony.Overture
   import Control.Lens  
   import Control.Lens.TH
   import Data.Comp.Derive
   import Language.C11.Syntax.Lens
   
   data Expr a where
-    Unary    :: { _operation :: a, _target :: a } -> Expr a
-    Binary   :: { _operation :: a, _left :: a, _right :: a } -> Expr a
-    Ternary  :: { _condition :: a, _left :: a, _right :: a} -> Expr a
+    Unary    :: { _operation :: ByteString, _target :: a } -> Expr a
+    Binary   :: { _operation :: ByteString, _left :: a, _right :: a } -> Expr a
+    Ternary  :: { _condition :: ByteString, _left :: a, _right :: a} -> Expr a
     Cast     :: { _left :: a, _right :: a} -> Expr a
     Index    :: { _left :: a, _right :: a } -> Expr a
     Call     :: { _target :: a, _arguments :: [a] } -> Expr a
-    Access   :: { _operation :: a, _left :: a, _right :: a } -> Expr a
+    Dot      :: { _target :: a, _identifier :: a } -> Expr a
+    Arrow    :: { _operation :: ByteString, _left :: a, _right :: a } -> Expr a
     Paren    :: { _target :: a } -> Expr a
   
   derive [ makeShowF
